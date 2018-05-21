cdae91240ed35c73d09ffb2ee67ecd420b9535e9
Ville Lehtonen <ville.lehtonen@siili.com>
2018-05-21 13:02:30 +0300

    teht 4.3 - 4.4

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,15 @@ const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogs) => {
+    var totalLikes = 0;
+    for(i = 0; i < blogs.length; i++) {
+        totalLikes += blogs[i].likes
+    }
+    return totalLikes
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
